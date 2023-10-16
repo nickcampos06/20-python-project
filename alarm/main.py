7c4a4ac5bc626cb721dab08b6310849bf176594b
pecacho214 <nickcampos06@gmail.com>
2023-10-15 20:47:26 -0400

    established alarm function

--- alarm/main.py
+++ alarm/main.py
@@ -2,8 +2,28 @@
 import time
 
 def set_alarm(alarm_time):
-    pass
+    hour_when_set = time.localtime().tm_hour
+    minute_when_set = time.localtime().tm_min
+    second_when_set = time.localtime().tm_sec
 
+    seconds_into_day_when_set = (hour_when_set * 3600) + (minute_when_set * 60) + second_when_set
+
+    hour_alarm_time = int(alarm_time[0])
+    minute_alarm_time = int(alarm_time[1])
+    second_alarm_time = int(alarm_time[2])
+
+    seconds_into_day_when_alarm = (hour_alarm_time * 3600) + (minute_alarm_time * 60) + second_alarm_time
+
+    if seconds_into_day_when_set > seconds_into_day_when_alarm:
+        seconds_into_day_when_alarm += 86400
+    
+    seconds_until_alarm = seconds_into_day_when_alarm - seconds_into_day_when_set
+
+    time.sleep(seconds_until_alarm)
+
+    print('Wake up!')
+    
 if __name__ == '__main__':
     alarm_time = input('Enter the time for the alarm (HH:MM:SS): ')
+    alarm_time = alarm_time.split(':')
     set_alarm(alarm_time)
